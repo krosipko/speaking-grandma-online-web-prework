c4014e7e6ba2f355d32a0630710c21379e656d87
Kyle Rosipko <krosipko@outlook.com>
2019-01-08 03:06:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -18,5 +18,7 @@ def speak_to_grandma(spoken)
   elsif spoken == spoken.upcase
     response = "NO, NOT SINCE 1938!"
     return response
+  else 
+    response = "HUH?! SPEAK UP SONNY!"
   end
 end
